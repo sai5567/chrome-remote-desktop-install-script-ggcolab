a4b32b272d2469a550a78b0e268c3ffc50a14c9d
sai5567 <62966928+sai5567@users.noreply.github.com>
2021-03-24 14:07:56 +0530

    Update CRD.sh

--- CRD.sh
+++ CRD.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 printf "Installing CRD-server... \nThis might take a while... " >&2
 {
-sudo useradd -m b4iterdev
-sudo adduser b4iterdev sudo
-echo 'b4iterdev:b4iter' | sudo chpasswd
+sudo useradd -m Sai
+sudo adduser Sai sudo
+echo 'Sai:root' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
@@ -18,17 +18,12 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser b4iterdev chrome-remote-desktop
+sudo adduser Sai chrome-remote-desktop
 } &> /dev/null &&
-printf "\nSetup Complete \nCreated new user b4iterdev with password:b4iter \nYou can change both b4iterdev's password with passwd(with no sudo) \nOr you can change root's password with sudo passwd" >&2 ||
+printf "\nSetup Complete \nCreated new user Sai with password:root \nYou can change both Sai's password with passwd(with no sudo) \nOr you can change root's password with sudo passwd" >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless and copy command of Debian Linux \n'
 read -p "Paste Here: " CRP
-su - b4iterdev -c """$CRP"""
+su - Sai -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
-if sudo apt-get upgrade &> /dev/null
-then
-    printf "\n\nUpgrade Completed " >&2
-else
-    printf "\n\nError Occured " >&2
-fi
+
